2244c601a5217e4921838b5b2627bf3cbd594bf7
Adhirajkadu <neelamkadu18@gmail.com>
2026-06-01 20:16:56 +0530

    Add files via upload

--- password.py
+++ password.py
@@ -1,19 +1,15 @@
 import random
-    
-number =random.randint(0, 20)
-chances= 0
-print("Guess passsword between 0000-1234, you only have 3 chance\n")
+import string
 
-while chances < 3:
-    guess=int(input("Guess your password :"))
+length=int(input("Enter the length of the password :"))
+characters=string.ascii_letters+string.digits
 
-    if guess == number:
-        print("YOU Opened the mobile !!!")
-        break
-    elif guess > number:
-        print("Your guess is too high, try lower number")
-    else:
-        print("Your guess is too low, try higher number")
-    chances += 1
-if not chances < 3:
-    print("YOU Didn't Open the mobile!!! the number is ",number)
+password=''
+for i in range(length):
+    password+=random.choice(characters)
+
+pass2=list(password)
+random.shuffle(pass2)
+password="".join(pass2)
+
+print("Generated password :",password)
